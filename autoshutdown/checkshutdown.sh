adfe9f0ee7e750feb598c704e4592843ce6d9345
Martin Unzner <martin.u@posteo.de>
2016-07-30 18:46:30 +0200

    Replace tabs with spaces

--- autoshutdown/checkshutdown.sh
+++ autoshutdown/checkshutdown.sh
@@ -7,25 +7,25 @@ set -u
 
 logit()
 {
-	logger -p local0.notice -s -- AutoShutdown: "$@"
+    logger -p local0.notice -s -- AutoShutdown: "$@"
 }
 
 IsBusy()
 {
-	USERCOUNT=$(who | wc -l);
-	if [[ $USERCOUNT -gt 0 ]]; then
+    USERCOUNT=$(who | wc -l);
+    if [[ $USERCOUNT -gt 0 ]]; then
         logit some users still connected, auto shutdown cancelled
         return 0
     fi
-	return 1
+    return 1
 }
 
 OFFFILE="/var/spool/shutdown_off"
 
 # turns off the auto shutdown
 if [ -e $OFFFILE ]; then
-	logit auto shutdown is turned off by existents of $OFFFILE
-	exit 0
+    logit auto shutdown is turned off by existents of $OFFFILE
+    exit 0
 fi
 
 if ! IsBusy; then
