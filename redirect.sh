b013953ff11fec0bb18ff49517f4385d00c98d76
Martin Unzner <martin.u@posteo.de>
2016-07-30 17:17:31 +0200

    Fix host name extraction

--- redirect.sh
+++ redirect.sh
@@ -4,7 +4,7 @@ set -e
 set -u
 
 OUTPUT=$(./start-tunnel.sh) || exit $?
-HOSTNAME=$(echo "$OUTPUT" | awk '/INSTANCE/ { print "$4" }') || exit $?
+HOSTNAME=$(echo "$OUTPUT" | awk '/INSTANCE/ { print $4 }') || exit $?
 echo "Host name: $HOSTNAME"
 echo
 ./open-tunnel.sh "$HOSTNAME" "$@"
