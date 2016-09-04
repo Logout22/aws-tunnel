9321fee2e59040d7b7450aaefe140d78b85b3a1b
Martin Unzner <martin.u@posteo.de>
2016-09-04 16:31:18 +0200

    Switch to Amazon Linux as Ubuntu broke down

--- open-tunnel.sh
+++ open-tunnel.sh
@@ -4,4 +4,4 @@ set -e
 set -u
 
 SCRIPTDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-ssh -D "${2:-8080}" -i "$SCRIPTDIR/Tunnel.pem" "ubuntu@$1"
+ssh -D "${2:-8080}" -i "$SCRIPTDIR/Tunnel.pem" "ec2-user@$1"
